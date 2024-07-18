862da2633dba828692130030db6955fcbae2108e
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 17:20:02 +0300

    Proje dosyası ekle.

---
+++ Kisi.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgoritmaTasarimi.Temeller
+{
+    internal class Kisi
+    {
+        private string v1;
+        private int v2;
+
+        public Kisi(string v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlgoritmaTasarimi.Temeller
+{
+    public class kisi
+    {
+        public kisi()
+        {
+        }
+
+        public kisi(string adi)
+        {
+            Adi = adi;
+        }
+
+        public kisi(string adi, int yas)
+        {
+            Adi = adi;
+            Yas = yas;
+        }
+
+        public string Adi { get; set; }
+        public int Yas { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kisi k = new kisi("Ahmet", 18);
+            Console.WriteLine(k.Adi + ":" + k.Yas);
+        }
+    }
+}
+
